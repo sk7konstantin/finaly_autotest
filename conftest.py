3db9ec1370e07f2a88c6ee73d8db262ab4f59f7a
Konstantin <sk7konstantin@yandex.ru>
2022-12-02 15:35:01 +0300

    Добавил выбор языка, переписал тест под объектную модель

--- conftest.py
+++ conftest.py
@@ -1,23 +1,54 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
         help='Choose browser: chrome or firefox')
 
-@pytest.fixture(scope="class")
-def browser(request):
-    browser_name = request.config.getoption('browser_name')
-    browser = None
+    parser.addoption('--language', action='store', default=None,
+        help='Choose laguage: ru or en')
+
+
+#todo Проверяем какой браузер выбрал пользователь и инициализируем его
+#! Все данные считываем из консоли
+def check_browser_name(browser_name, options):
     if browser_name == 'chrome':
         print("\nstart browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=options)
     elif browser_name == 'firefox':
         print('Install Firefox driver')
     else:
         raise pytest.UsageError('--browser_name should be chrome or firefox')
-    browser.implicitly_wait(5)
+    return browser
+
+
+#todo Проверяем какой язык выбрал пользователь и передаем его в опции браузера
+#! Все данные считываем из консоли
+def check_language_user(user_language, options):
+    if user_language == 'ru':
+        options.add_experimental_option('prefs', {'intl.accept_languages': 'ru'})
+    elif user_language == 'en':
+        options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
+    else:
+        raise pytest.UsageError('--language should be ru or en')
+    
+    return options
+
+#todo Инициализируем браузер и добавляем в него выбранные опции
+@pytest.fixture(scope="class")
+def browser(request):
+    user_language = request.config.getoption('language')
+    browser_name = request.config.getoption('browser_name')
+
+    browser = None
+    options = Options()
+    options = check_language_user(user_language, options)
+
+    browser = check_browser_name(browser_name, options)
+
     yield browser
+    
     print("\nquit browser..")
     browser.quit()
--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,12 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
-    login_link.click()
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
-    time.sleep(5)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    time.sleep(2)
     
